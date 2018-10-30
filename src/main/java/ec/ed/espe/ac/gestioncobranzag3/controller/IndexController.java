baf5c7b114a5d1c7d902f2cbae82b0278c4cea6d
JhonathanALM <jalechon@espe.edu.ec>
2018-10-30 18:36:55 -0500

    Creacion del proyecto y Login

---
+++ src/main/java/ec/ed/espe/ac/gestioncobranzag3/controller/IndexController.java
@@ -0,0 +1,57 @@
+package ec.ed.espe.ac.gestioncobranzag3.controller;
+import ec.edu.espe.ac.gestioncobranzasg3.ejb.UsuarioFacade;
+import ec.edu.espe.ac.model.Usuario;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ * @author jhona
+ */
+@Named
+@ViewScoped
+public class IndexController implements Serializable {
+    @EJB
+    private UsuarioFacade EJBUsuario;
+    private Usuario usuario;
+    
+    @PostConstruct
+    public void init (){
+        usuario = new Usuario();
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    public String iniciarSesion(){
+        String redireccion="";
+        Usuario us;
+        try{
+            us = EJBUsuario.iniciarSesion(usuario,usuario.getId() );
+            if(us!=null){
+                redireccion="/inicio?faces-redirect=true";
+                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", us);
+            }else{
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Aviso","Credenciales Incorrectas"));
+                System.out.println("Fallamos prro :( ");
+            }
+            
+        }catch(Exception e){
+            System.out.println("Error: "+usuario.getId());
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"Aviso","ERROR!"));
+        }
+        return redireccion;
+    }
+    public void cerrarSesion(){
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+    }
+}
---
+++ src/main/java/ec/edu/espe/ac/gestioncobranzasg3/ejb/UsuarioFacade.java
@@ -0,0 +1,49 @@
+package ec.edu.espe.ac.gestioncobranzasg3.ejb;
+
+import ec.edu.espe.ac.model.Usuario;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ * @author jhona
+ */
+@Stateless
+public class UsuarioFacade extends AbstractFacade<Usuario> {
+
+    @PersistenceContext(unitName = "ec.edu.espe.ac_GestionCobranzasG3_war_1PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UsuarioFacade() {
+        super(Usuario.class);
+    }
+         public Usuario iniciarSesion(Usuario us, String usu) {
+        Usuario usuario = null;
+        String consulta;
+        try{
+             consulta= "FROM Usuario u WHERE u.id =?1";
+            Query query = em.createQuery(consulta);
+            System.out.println(usu+" ---- ");
+            query.setParameter(1, usu);
+            List<Usuario> lista = query.getResultList();
+            
+            if(!lista.isEmpty()){
+                usuario=lista.get(0);
+            }
+            
+        }catch(Exception e){
+            System.out.println("ERROR!!!"+e);
+        }finally{
+            //
+        }
+        return usuario;
+    }
+
+}
